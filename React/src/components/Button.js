2318ec08d6af903c879a0eef1c2442876e9afe48
mystardious <47038554+mystardious@users.noreply.github.com>
2020-10-05 04:04:09 +1100

    Changed how list of tasks are displayed
    Tasks are listed in card format rather than in a table format.

---
+++ React/src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/** 
+ * Button to apply for tasks
+ * @param data Data about the task
+ * @param callBack Api method to call when submitting request
+ */
+export class ApplyButton extends React.Component {
+    render() {
+
+        let render;
+
+        console.log(this.props.data)
+
+        if(this.props.data.isRegistered)
+            render = <div style={{color: 'green'}}>You have already applied!</div>;
+
+        else
+            render =
+            <form onSubmit={this.props.callBack} className="button">
+                <input type="hidden" value={this.props.taskID} name="id" />
+                <input type="submit" value="Apply for Task" />
+            </form>
+
+        return render
+    }
+}
