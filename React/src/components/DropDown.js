04c7a613ece5bbac8e93d94d5bab7b8a419b4449
DESKTOP-T1TG7TR\Ahmad's Laptop <ahmed.sikdr@gmail.com>
2020-10-06 15:09:23 +1100

    Fixed log out functionality

--- React/src/components/DropDown.js
+++ React/src/components/DropDown.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
 export default function SimpleMenu(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null)
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
+
   const handleLogout = () => {
-  fetch('/logout',{method:'POST'},(req,res) => {
-  res.redirect('/');
-  })
-  };
+    fetch('/logout', { method: 'POST' }).then(handleRedirect)
+  }
+
+  const handleRedirect = (e) => {
+    if (e.status == 200) window.location.href = '/'
+    else console.log(e)
+  }
 
   return (
     <div>
-      <Button aria-controls="simple-menu" style={{color: 'rgba(255, 255, 255, 0.5)', fontWeight: 'bold'}} aria-haspopup="true" onClick={handleClick}>
-        {props.authenticated} &nbsp;<AccountCircleIcon />
+      <Button
+        aria-controls="simple-menu"
+        style={{ color: 'rgba(255, 255, 255, 0.5)', fontWeight: 'bold' }}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        {props.authenticated} &nbsp;
+        <AccountCircleIcon />
       </Button>
       <Menu
         id="simple-menu"
@@ -33,11 +42,12 @@ export default function SimpleMenu(props) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <a href="/accountProfile"><MenuItem onClick={handleClose}>Profile</MenuItem></a>
+        <a href="/accountProfile">
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+        </a>
         <MenuItem onClick={handleClose}>My account</MenuItem>
         <MenuItem onClick={handleLogout}>Logout</MenuItem>
-
       </Menu>
     </div>
-  );
+  )
 }
